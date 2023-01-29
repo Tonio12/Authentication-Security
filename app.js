d32c41cd667349a3086a4f94ce9dc7309431f7e8
Tonio12 <nelson.antonio.an@gmail.com>
2023-01-29 12:15:51 +0000

    Level 3: Adding Environment Variable

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs =  require('ejs');
@@ -16,8 +17,8 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-const secretKey = 'thisismyvarylongencryptionkey';
-userSchema.plugin(encrypt, {secret: secretKey, encryptedFields: ['password']});
+
+userSchema.plugin(encrypt, {secret: process.env.SECRETKEY, encryptedFields: ['password']});
 
 const User = mongoose.model('User', userSchema);
 
